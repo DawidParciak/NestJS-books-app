e960b60950ec472b943be6c592fbcd7787ace754
Dawid <daw.parc@gmail.com>
2023-06-25 21:25:33 +0200

    Add registration

--- src/authors/dtos/create-author.dto.ts
+++ src/authors/dtos/create-author.dto.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateAuthorDTO {
---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+
+export class CreateUserDTO {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string
+
+  @IsNotEmpty()
+  @IsString()
+  password: string
+}
